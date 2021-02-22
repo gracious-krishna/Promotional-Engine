6399e2b768f39b73eb942ede44e1cbe147c18983
gracious-krishna <78645473+gracious-krishna@users.noreply.github.com>
2021-02-22 23:44:56 +0100

    Scenario created

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,18 +1,23 @@
 using NUnit.Framework;
+using PromotionalEngine.Core;
 
 namespace Promotional_Engine
 {
-    public class Tests
+  public class Tests
+  {
+    private Cart CartScenario1 { get; set; }
+    private Cart CartScenario2 { get; set; }
+    private Cart CartScenario3 { get; set; }
+    
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
     }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
 }
