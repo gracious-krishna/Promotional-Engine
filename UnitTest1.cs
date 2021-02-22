3022aaced8a68c404706a57db30b87ec00582490
gracious-krishna <78645473+gracious-krishna@users.noreply.github.com>
2021-02-22 23:47:11 +0100

    Price of different SKU

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,10 +8,18 @@ namespace Promotional_Engine
     private Cart CartScenario1 { get; set; }
     private Cart CartScenario2 { get; set; }
     private Cart CartScenario3 { get; set; }
-    
+
     [SetUp]
     public void Setup()
     {
+      CartScenario1 = new Cart();
+      CartScenario2 = new Cart();
+      CartScenario3 = new Cart();
+
+      var a = new Item("A", 50);
+      var b = new Item("B", 30);
+      var c = new Item("C", 20);
+      var d = new Item("D", 15);
     }
 
     [Test]
