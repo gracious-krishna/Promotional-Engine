174575456a100d7a9517f87189200d27e7ace94b
gracious-krishna <78645473+gracious-krishna@users.noreply.github.com>
2021-02-22 23:13:39 +0100

    AddItem and AddPromotions methods

--- Cart.cs
+++ Cart.cs
@@ -16,6 +16,23 @@ namespace PromotionalEngine.Core
     public List<Promotion> Promotions { get; set; }
     public decimal Total { get; set; }
 
-    
+    public void AddItem(CartItem item)
+    {
+      Items.Add(item);
+      CalculateTotal();
+    }
+    public void AddPromotion(Promotion promo)
+    {
+      Promotions.Add(promo);
+      CalculateTotal();
+    }
+    public void AddPromotions(List<Promotion> promos)
+    {
+      Promotions.AddRange(promos);
+      CalculateTotal();
+    }
+
+    private void CalculateTotal()
+    {}
   }
 }
