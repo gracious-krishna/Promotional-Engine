d396b34b4145773a9562df149d30d6d7204e854e
gracious-krishna <78645473+gracious-krishna@users.noreply.github.com>
2021-02-22 23:07:03 +0100

    Price For Quantity Promotion

--- Promotions.cs
+++ Promotions.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 
 namespace PromotionalEngine.Core
 {
-    public class Promotion
-    {        
-        public PromotionType Type { get; set; }
-    }
+  public class Promotion
+  {
+    public PromotionType Type { get; set; }
+  }
 
-    public enum PromotionType
+  public class PriceForQuantityPromotion : Promotion
+  {
+    public Item Item { get; set; }
+    public int Quantity { get; set; }
+    public decimal Total { get; set; }
+    public PriceForQuantityPromotion(Item item, int quantity, decimal total)
     {
-        PriceForQuantityPromotion = 1, 
-        ComboPromotion = 2
+      Item = item;
+      Quantity = quantity;
+      Total = total;
+      Type = PromotionType.PriceForQuantityPromotion;
     }
+  }
+  
+  public enum PromotionType
+  {
+    PriceForQuantityPromotion = 1,
+    ComboPromotion = 2
+  }
 }
